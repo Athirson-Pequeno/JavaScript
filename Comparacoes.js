8f886e71a2b33bb91d35eeb4b0c1fbcb1d18b9ee
Athirson-Pequeno <athirsondev@gmail.com>
2022-08-09 10:24:42 -0300

    comparacoes

---
+++ Comparacoes.js
@@ -0,0 +1,16 @@
+// == comparação implicita
+
+const numero = 5;
+const texto = "5"
+
+console.log(numero == texto)
+
+// === comparação explicita
+
+console.log(numero === texto)
+
+//typeof 
+
+console.log(typeof numero)
+console.log(typeof texto)
+console.log(typeof (numero==texto))
