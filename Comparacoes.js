fd7be38183fe60050ee0279e85048b1a646ba9a8
Athirson-Pequeno <athirsondev@gmail.com>
2022-08-09 10:33:00 -0300

    operador ternario

--- Comparacoes.js
+++ Comparacoes.js
@@ -1,11 +1,11 @@
-// == comparação implicita
+// == comparação implicita - compara so o valor
 
 const numero = 5;
 const texto = "5"
 
 console.log(numero == texto)
 
-// === comparação explicita
+// === comparação explicita - compara o valor e o tipo
 
 console.log(numero === texto)
 
@@ -14,3 +14,6 @@ console.log(numero === texto)
 console.log(typeof numero)
 console.log(typeof texto)
 console.log(typeof (numero==texto))
+
+//conversão explicita usando Number() e String(), priorizar o uso de === 
+
---
+++ Operador-ternario.js
@@ -0,0 +1,8 @@
+//operador ternario
+//funciona como um if e else
+
+const idadeMinima = 18;
+const idadeCliente = 21;
+
+                    //condição              true        false
+console.log(idadeCliente >= idadeMinima ? "cerveja"  :  "suco")
