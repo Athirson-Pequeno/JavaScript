3f0119738651ee79be8cb4a00897293786278478
Athirson-Pequeno <athirsondev@gmail.com>
2022-08-09 10:49:56 -0300

    template string

---
+++ Template-string.js
@@ -0,0 +1,8 @@
+const nome = "Athirson";
+const idade = 2022-2000;
+const cidade = "Campina Grande"
+
+const apresentacao = `meu nome é ${nome}, minha idade é ${idade} e nasci na cidade de ${cidade}`;
+
+console.log(apresentacao)
+
