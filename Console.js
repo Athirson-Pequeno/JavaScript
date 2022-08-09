86316a841317cd0aeddf5872d579a8c94e7b86cb
Athirson-Pequeno <athirsondev@gmail.com>
2022-08-09 10:10:19 -0300

    aula de console

---
+++ Console.js
@@ -0,0 +1,11 @@
+//log -> registro
+
+const minhaVar = true;
+
+console.log(245)
+console.log("registro")
+console.log(minhaVar)
+
+//error -> mostra um erro previsto, tratamento de erro
+
+console.error('deu erro!')
