72ea7bf0aa102587ac88ce5d07e11a7314431752
Athirson-Pequeno <athirsondev@gmail.com>
2022-08-09 10:54:09 -0300

    template string

--- Template-string.js
+++ Template-string.js
@@ -1,8 +1,16 @@
 const nome = "Athirson";
 const idade = 2022-2000;
-const cidade = "Campina Grande"
+const cidade = "Campina Grande";
+
+const nomePedido = "Leo";
+const idadePedido = 23;
+const bebidaMaior = "cerveja";
+const bebidaMenor = "suco";
 
 const apresentacao = `meu nome é ${nome}, minha idade é ${idade} e nasci na cidade de ${cidade}`;
 
 console.log(apresentacao)
 
+
+const pedido = `${nome} diz: "por favor, quero beber ${idade >= 18 ? bebidaMaior : bebidaMenor}"`
+console.log(pedido)
