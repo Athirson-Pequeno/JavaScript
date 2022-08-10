2541526f7687db6622f281dab408fdfc29c03ced
Athirson-Pequeno <athirsondev@gmail.com>
2022-08-10 09:36:04 -0300

    funções

---
+++ Funcoes.js
@@ -0,0 +1,31 @@
+let x = "";
+console.log(x);
+x = "oi";
+
+
+//declaração de função---
+//chama a função
+imprimeTexto(x);
+
+//cria a função
+function imprimeTexto(texto){
+    console.log(texto)
+}
+
+//é possivel chamar a função em qualquer lugar
+imprimeTexto("chamei de novo");
+//---
+
+
+//função com retorno---
+function soma(){
+const resultado = 2 + 2;
+return resultado;
+}
+
+console.log(soma());
+//---
+
+//função dentro de função---
+imprimeTexto(soma());
+//---
